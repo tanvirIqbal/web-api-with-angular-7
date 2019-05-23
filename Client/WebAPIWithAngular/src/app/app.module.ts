be96abf6b1ed75e0f84bf750924d6586c2b73838
tanviriqbal <tanviriqbal4u@live.com>
2019-05-23 23:20:27 +0600

    Add Employee Model and Register Employee Service in app.module.ts

--- Client/WebAPIWithAngular/src/app/app.module.ts
+++ Client/WebAPIWithAngular/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
 import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { EmployeeService } from './shared/employee.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { EmployeeListComponent } from './employees/employee-list/employee-list.c
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
