d5df811bdb5b80fd30946b989d2aba0cda352895
tanviriqbal <tanviriqbal4u@live.com>
2019-05-23 19:55:40 +0600

    Create Web API project

---
+++ Server/WebAPIWithAngular/WebAPIWithAngular/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebAPIWithAngular.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Server/WebAPIWithAngular/WebAPIWithAngular/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPIWithAngular.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
