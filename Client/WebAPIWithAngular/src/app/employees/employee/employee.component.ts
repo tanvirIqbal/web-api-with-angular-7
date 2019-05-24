693f9154e404a887dd3760ee7414cc1c5551fa49
tanviriqbal <tanviriqbal4u@live.com>
2019-05-24 09:27:52 +0600

    Create Employee Form with template driven aproach

--- Client/WebAPIWithAngular/src/app/employees/employee/employee.component.ts
+++ Client/WebAPIWithAngular/src/app/employees/employee/employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/shared/employee.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
@@ -11,6 +12,20 @@ export class EmployeeComponent implements OnInit {
   constructor(private service: EmployeeService) { }
 
   ngOnInit() {
+    this.resetForm();
+  }
+
+  resetForm(form?: NgForm) {
+    if (form != null) {
+      form.resetForm();
+    }
+    this.service.formData = {
+      EmployeeID: null,
+      FullName: '',
+      Position: '',
+      EmployeeNo: '',
+      Mobile: ''
+    };
   }
 
 }
